19ac3d6d696c10c1db8b50c490522de3cc44be1e
Jas-974 <v.jasmin@orange.fr>
2026-03-02 22:45:56 +0400

    Traitement du formulaire de connexion

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+// Vider les variables de session
+$_SESSION = [];
+
+// Détruire la session
+session_destroy();
+
+// Redirection vers la page de connexion
+header("Location: /dev_web_locachat/index_cnxn_creacompte.php");
+exit;
