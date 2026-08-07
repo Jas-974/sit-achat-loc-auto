0f88501e7c8fe9ff1da12cf6e788224bc73fa265
Jas-974 <v.jasmin@orange.fr>
2026-08-07 21:40:01 +0400

    Mise à jour du test commande véhicule location

--- Tests/enregCommandeVehiculeLocationTest.php
+++ Tests/enregCommandeVehiculeLocationTest.php
@@ -15,7 +15,8 @@ class enregCommandeVehiculeLocationTest extends TestCase
     $_POST = [];
 
     $this->pdo = new PDO('sqlite::memory:');
-    $this->pdo->exec("
+    $this->pdo->exec(
+      "
 CREATE TABLE table_statu_command (
 id INTEGER PRIMARY KEy,
 numero_command TEXT,
@@ -24,11 +25,13 @@ prenom TEXT,
 email TEXT, 
 type_offre TEXT, 
 status_command TEXT, 
-user_id TEXT
+user_id TEXT,
+ code_status_command INTEGER,
+commande_id INTEGER
 )"
-);
+    );
 
- $this->pdo->exec("
+    $this->pdo->exec("
  CREATE TABLE table_commandes (
 id INTEGER PRIMARY KEy,
 user_id INTEGER, 
@@ -38,7 +41,7 @@ documents TEXT,
 adate TEXT)
 ");
 
-$this->pdo->exec("
+    $this->pdo->exec("
 CREATE TABLE documents_upload (
 id INTEGER PRIMARY KEy,
 user_id INTEGER, 
@@ -46,43 +49,41 @@ car_id INTEGER,
 documents TEXT,
 created_at TEXT)
 ");
+  }
 
+  public function testEnregCommandeLocation()
+  {
+    $_POST["maj_status_command"] = "Réservation en cours";
+
+    $donnee_user = [
+      "nom" => "Robert",
+      "prenom" => "Leto",
+      "email" => "leto.Rob@orange.fr"
+    ];
+
+    $donnee_vehicule = [
+      "id" => 1,
+      "type_offre" => "location"
+    ];
+
+    $res_enreg_command = enregCommandeVehiculeLocation($this->pdo, $donnee_user, $donnee_vehicule, 4);
+    $this->assertIsNumeric($res_enreg_command);
+
+    $stmt = $this->pdo->query("SELECT * FROM table_statu_command");
+    $res_status = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+    $this->assertEquals("Robert", $res_status["nom"]);
+    $this->assertEquals("Leto", $res_status["prenom"]);
+    $this->assertEquals("leto.Rob@orange.fr", $res_status["email"]);
+    $this->assertEquals("location", $res_status["type_offre"]);
+    $this->assertEquals("Réservation en cours", $res_status["status_command"]);
+
+
+    $stmt = $this->pdo->query("SELECT * FROM table_commandes");
+    $la_command = $stmt->fetch(PDO::FETCH_ASSOC);
+    $this->assertEquals(4, $la_command["user_id"]);
+    $this->assertEquals(1, $la_command["car_id"]);
+    $this->assertEquals("location", $la_command["order_type"]);
+  }
 }
-
-public function testEnregCommandeLocation()
-{
-$_POST["maj_status_command"] = "Réservation en cours";
-
-$donnee_user = [
-"nom" => "Robert",
-"prenom" => "Leto",
-"email" => "leto.Rob@orange.fr"
-];
-
-$donnee_vehicule = [
-"id" => 1,
-"type_offre" => "location"
-];
-
-$res_enreg_command = enregCommandeVehiculeLocation($this->pdo, $donnee_user, $donnee_vehicule, 4);
-$this->assertIsNumeric($res_enreg_command);
-
-$stmt = $this->pdo->query("SELECT * FROM table_statu_command");
-$res_status = $stmt->fetch(PDO::FETCH_ASSOC);
-
-
-$this->assertEquals("Robert", $res_status["nom"]);
-$this->assertEquals("Leto", $res_status["prenom"]);
-$this->assertEquals("leto.Rob@orange.fr", $res_status["email"]);
-$this->assertEquals("location", $res_status["type_offre"]);
-$this->assertEquals("Réservation en cours", $res_status["status_command"]);
-
-
-$stmt = $this->pdo->query("SELECT * FROM table_commandes");
-$la_command = $stmt->fetch(PDO::FETCH_ASSOC);
-$this->assertEquals(4, $la_command["user_id"]);
-$this->assertEquals(1, $la_command["car_id"]);
-$this->assertEquals("location", $la_command["order_type"]);
-}
-}
-?>
